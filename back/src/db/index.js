c8d61144a5e1ad65fd9310a32fe7e2bddb1aca2c
김현정 <khjung11203@gmail.com>
2022-08-26 16:22:42 +0900

    김현정: 수상 이력 api 추가

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
 import { Education } from "./models/Education";
+import { Award } from "./models/Award";
 import { Certificate } from "./models/Certificate";
 
 const DB_URL =
@@ -21,4 +22,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Education, Certificate };
+export { User, Education, Certificate, Award };

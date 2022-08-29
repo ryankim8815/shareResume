da28df28a2a9e590d1f11fe37483fd3f52580683
yangjisu <yangjisue@gmail.com>
2022-08-29 16:52:10 +0900

    change edu_id->eduId

--- front/src/components/education/EducationCard.js
+++ front/src/components/education/EducationCard.js
@@ -3,16 +3,18 @@ import { Card, Row, Button, Col } from "react-bootstrap";
 function EducationCard({ edu, setIsEditing, isEditable }) {
   return (
     <Card.Text>
-      <Row>
-       <Col xl={11}>
+      <Row className="justify-content-between align-items-center mb-2">
+       <Col>
           {edu.school}
           <br />
           {edu.major} ({edu.degree})
         </Col>
       {isEditable && (
-        <Col xl={1}>
+        <Col  xs lg="1">
           <Button
             variant="outline-info"
+            size="sm"
+            className="mr-3"
             onClick={() =>
               setIsEditing((position) => {
                 return !position;})}>

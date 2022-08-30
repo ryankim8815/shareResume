6b98e26b09102b93c5e8310065d9909e02fc1697
hyeonki KIM <khk5676@gmail.com>
2022-08-30 11:53:08 +0900

    project delete complete

--- front/src/components/project/ProjectCard.js
+++ front/src/components/project/ProjectCard.js
@@ -1,6 +1,17 @@
 import { Card, Button, Row, Col } from "react-bootstrap";
 
-function ProjectCard({ project, isEditable, setIsEditing }) {
+function ProjectCard({ project, isEditable, setIsEditing, setProjects}) {
+  async function handleDelete() {
+    const id = project.id
+    try{
+      await Api.delete(`project/${project.projId}`);
+      const res = await Api.get(`project`, id);
+      setProjects(res.data);
+    }catch(error){
+      console.log(error);
+    }
+  }
+  
   return (
     <Card.Text>
       <Row className="justify-content-between align-items-center mb-2">
@@ -23,6 +34,14 @@ function ProjectCard({ project, isEditable, setIsEditing }) {
             >
               편집
             </Button>
+            <Button
+            variant="outline-danger"
+            size="sm"
+            className="mr-3"
+            onClick={handleDelete}
+            >
+            삭제
+          </Button>
           </Col>
         )}
       </Row>

90ede0256bc247de4aa0c2ddd24e959a2e516a5f
yangjisu <yangjisue@gmail.com>
2022-08-31 11:29:14 +0900

    add UserTable in Network

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, Row } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Form,
+  ButtonGroup,
+  Button,
+  Col,
+  Table,
+} from "react-bootstrap";
 
 import * as Api from "../../api";
 import UserCard from "./UserCard";
 import { UserStateContext } from "../../App";
-
+import UserTable from "./UserTable";
 function Network() {
   const navigate = useNavigate();
   const userState = useContext(UserStateContext);
   // useState 훅을 통해 users 상태를 생성함.
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [showCard, setShowCard] = useState(true);
+  function toggleShow() {
+    setShowCard(!showCard);
+  }
 
   useEffect(() => {
     // 만약 전역 상태의 user가 null이라면, 로그인 페이지로 이동함.
@@ -23,13 +36,82 @@ function Network() {
   }, [userState, navigate]);
 
   return (
-    <Container fluid>
-      <Row xs="auto" className="jusify-content-center">
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} isNetwork />
-        ))}
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <Col md={{ span: 1, offset: 5 }}>
+            <Form className="mb-3 ms-3 mr-10">
+              <input
+                type="text"
+                value={search}
+                placeholder="검색"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </Form>
+          </Col>
+          <Col md={{ span: 1, offset: 5 }}>
+            <ButtonGroup aria-label="Basic example">
+              <Button
+                variant="secondary"
+                onClick={toggleShow}
+                style={showCard ? { color: "navy" } : { color: "white" }}
+              >
+                Card
+              </Button>
+              <Button
+                variant="secondary"
+                onClick={toggleShow}
+                style={!showCard ? { color: "navy" } : { color: "white" }}
+              >
+                Table
+              </Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
+      </Container>
+      {showCard ? (
+        <Container fluid>
+          <Row xs="auto" className="justify-content-md-center">
+            {users
+              .filter((data) => {
+                if (search === "") {
+                  return data;
+                } else if (data.name.includes(search)) {
+                  return data;
+                }
+              })
+              .map((user) => {
+                return <UserCard key={user.id} user={user} isNetwork />;
+              })}
+          </Row>
+        </Container>
+      ) : (
+        <Container fluid="md">
+          <Table className="mb-2 ms-3 mr-5" style={{ textAlign: "center" }}>
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Description</th>
+                <th>Portfolio</th>
+              </tr>
+            </thead>
+            {users
+              .filter((data) => {
+                if (search === "") {
+                  return data;
+                } else if (data.name.includes(search)) {
+                  return data;
+                }
+              })
+              .map((user) => {
+                return <UserTable key={user.id} user={user} isNetwork />;
+              })}
+          </Table>
+        </Container>
+      )}
+    </>
   );
 }
 

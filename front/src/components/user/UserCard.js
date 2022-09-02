03d5959978db153f293218d191131d2809b2c502
ryankim <ryankim8815@gmail.com>
2022-09-02 15:38:16 +0900

    프로필 이미지 경로 수정

--- front/src/components/user/UserCard.js
+++ front/src/components/user/UserCard.js
@@ -6,24 +6,19 @@ import "../components.css";
 
 //test
 
-function UserCard({
-  user,
-  setIsEditing,
-  isEditable,
-  isNetwork,
-}) {
+function UserCard({ user, setIsEditing, isEditable, isNetwork }) {
   const navigate = useNavigate();
 
   return (
     <Card className="mb-2 ms-3 mr-5 profile-card" style={{ width: "18rem" }}>
       <Card.Body>
         <Row className="justify-content-md-center">
-            <Card.Img
-              style={{ width: "10rem", height: "8rem" }}
-              className="mb-3"
-              src={`http://localhost:5001/${user?.profileImageFilename}`}
-              alt="사용자 등록 프로필 이미지"
-            />
+          <Card.Img
+            style={{ width: "10rem", height: "8rem" }}
+            className="mb-3"
+            src={`http://kdt-ai5-team11.elicecoding.com:5001/${user?.profileImageFilename}`}
+            alt="사용자 등록 프로필 이미지"
+          />
         </Row>
         <Card.Title>{user?.name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{user?.email}</Card.Subtitle>
@@ -33,10 +28,7 @@ function UserCard({
           <Col>
             <Row className="mt-3 text-center text-info">
               <Col sm={{ span: 20 }}>
-                <button
-                  className="edit-btn"
-                  onClick={() => setIsEditing(true)}
-                >
+                <button className="edit-btn" onClick={() => setIsEditing(true)}>
                   편집
                 </button>
               </Col>
